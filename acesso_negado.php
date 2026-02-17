6c3fab6a97ef150baa0876d42b1e0894c6ffd05f
Huelton Santos (DEv) <hueltonti@gmail.com>
2026-02-17 09:45:35 -0300

    Update acesso_negado.php

---
+++ acesso_negado.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Acesso Negado</title>
+    <!-- SB Admin 2 CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/startbootstrap-sb-admin-2@4.1.4/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" type="text/css">
+</head>
+<body class="bg-light">
+    <div class="container text-center" style="margin-top: 100px;">
+        <div class="error mx-auto" data-text="403">403</div>
+        <p class="lead text-gray-800 mb-5">Acesso Negado</p>
+        <p class="text-gray-500 mb-0">Parece que você não tem permissão para acessar esta página.</p>
+        <a href="index.php">&larr; Voltar para o Dashboard</a>
+    </div>
+</body>
+</html>
